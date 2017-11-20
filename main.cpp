0123c69ae7f50e0742f420553c4b2758dcc459c2
ming5656 <s110168@shsh.tw>
2017-11-21 00:47:10 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+ll p;
+ll num[45];
+
+ll invert(ll num)
+{
+    ll q = p-2,ans(1),base = num;
+    while(q>0)
+    {
+        if(q&1)
+        {
+            ans= (ans *base)% p;
+        }
+        q>>=1;
+        base = (base *base)% p;
+    }
+    return ans;
+}
+struct number
+{
+    ll value,times;
+    bool operator <(const number &b) const
+    {
+        return value<b.value;
+    }
+};
+ll dp(int n)
+{
+    vector<number> product ,buffer;
+    number start ={1,1};
+    product.push_back(start);
+    for(int i=0;i<n;i++)
+    {
+        buffer.clear();
+        vector<number> :: iterator it;
+        for(int j=0;j<product.size();j++)
+        {
+            number d;
+            d.value = (num[i] * product[j].value)%p;
+            d.times = product[j].times;
+            if(d.value==0)
+                continue;
+            else if(buffer.size()==0)
+            {
+                buffer.push_back(d);
+            }
+            else
+            {
+                it = lower_bound(buffer.begin(),buffer.end(),d);
+                if(it->value==d.value)
+                {
+                    it->times+=d.times;
+                }
+                else if(it==buffer.end())
+                {
+                    buffer.push_back(d);
+                }
+                else
+                {
+                    buffer.insert(it,d);
+                }
+            }
+        }
+        for(int j=0;j<buffer.size();j++)
+        {
+            it = lower_bound(product.begin(),product.end(),buffer[j]);
+            if(it==product.end())
+            {
+                product.push_back(buffer[j]);
+            }
+            else if(it->value==buffer[j].value)
+            {
+                it->times+=buffer[j].times;
+            }
+            else
+            {
+                product.insert(it,buffer[j]);
+            }
+        }
+    }
+    return product[0].times-1;
+}
+int main()
+{
+    int t,n;
+    scanf("%d",&t);
+    while(t--)
+    {
+        scanf("%d%lld",&n,&p);
+        for(int i=0;i<n;i++)
+        {
+            scanf("%lld",&num[i]);
+            num[i]=invert(num[i]);
+        }
+        printf("%lld\n",dp(n));
+    }
+    return 0;
+}
